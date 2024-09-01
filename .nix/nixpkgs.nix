66251799b3a26bb8f389f7a1625cf374d2c1b239
Ali Caglayan <alizter@gmail.com>
2024-09-01 12:23:58 +0300

    coq-lsp: 0.1.9 -> 0.2.0
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    
    <!-- ps-id: f1cc148c-a782-485e-beda-dc8c4f603b86 -->

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/8ffc337b2122a1a7deb47ae987f466e6208d8a81.tar.gz;
-    sha256 = "12n1vnj04a1g37hhisnq90mxr933xjqhzddh4c6q1scqc9cqb026";
-  }
+         url = https://github.com/Alizter/nixpkgs/archive/1f28777ea5b706088efdc5b5b8bb0e2da4df3d12.tar.gz;
+         sha256 = "1ma1wlngmf99apnjjy4g43f5xpzhjb59x6n4q8jjb3vx3wmb51rz";
+       }
